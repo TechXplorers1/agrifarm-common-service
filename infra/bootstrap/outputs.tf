48cdcdcf6a32cb6ee5973955d004755d85612627
gopivr <gopiravi@Gopis-MacBook-Air.local>
2026-06-28 17:28:13 -0400

    pipeline change

--- infra/bootstrap/outputs.tf
+++ infra/bootstrap/outputs.tf
@@ -5,3 +5,11 @@ output "ecr_repository_url" {
 output "ecr_repository_name" {
   value = aws_ecr_repository.ecr.name
 }
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "common_service_tg_arn" {
+  value = aws_lb_target_group.common_service_tg.arn
+}
