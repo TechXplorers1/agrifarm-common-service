6228eb16e68ef1caedc14fdb4d60ede11031e6e4
GopiRavi <gopivr@hotmail.com>
2026-07-22 21:24:36 -0400

    Adding RequestLogging Filter

---
+++ src/main/java/com/agrifarms/common/config/RequestLoggingFilter.java
@@ -0,0 +1,26 @@
+package com.agrifarms.common.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        System.out.println("Content-Type: " + request.getContentType());
+
+        filterChain.doFilter(request, response);
+    }
+}
