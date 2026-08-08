c84b1d2124d0f237b93646084248033541352182
Bharath Surya <humer@techxplorers.in>
2026-08-08 16:56:14 +0530

    updated the sprayers

--- src/main/java/com/agrifarms/common/entity/User.java
+++ src/main/java/com/agrifarms/common/entity/User.java
@@ -1,10 +1,16 @@
 package com.agrifarms.common.entity;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 import org.hibernate.annotations.UuidGenerator;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
