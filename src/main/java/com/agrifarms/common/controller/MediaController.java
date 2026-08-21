6ff74e6ec126e09a8ecd6b5af3dbe2f588474827
Bharath Surya <humer@techxplorers.in>
2026-08-21 17:04:20 +0530

    updated the bugs from backend

--- src/main/java/com/agrifarms/common/controller/MediaController.java
+++ src/main/java/com/agrifarms/common/controller/MediaController.java
@@ -1,11 +1,19 @@
 package com.agrifarms.common.controller;
 
-import com.agrifarms.common.service.MediaService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.agrifarms.common.service.MediaService;
 
 @RestController
 @RequestMapping("/api/media")
